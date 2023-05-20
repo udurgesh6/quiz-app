747c6e035300925fe2e5c4e5574fb29d7a402289
Durgesh Upadhyay <udurgesh6@gmail.com>
2023-05-20 18:25:50 +0530

    api details

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import { useState, useEffect, useContext } from "react";
 import Quiz from "./assets/quiz.png";
 import Modal from "./Modal";
 import { QuizContextProvider } from "./contexts/QuizContext";
+import axios from "axios";
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
+      })
+      .catch(() => {
+        alert("Something went wrong!");
+      })
+      .finally(() => {
+        console.log("I will run anyway");
+      });
+  }, []);
+
+  const getParticularProductDetail = (id) => {
+    axios.get(`https://fakestoreapi.com/products/${id}`).then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <QuizContextProvider>
       <div className="h-screen flex flex-col items-center justify-center ">
@@ -12,6 +35,14 @@ function App() {
             Interactive Quiz
           </p>
           <Modal />
+          {products.map((product) => (
+            <div
+              key={product.id}
+              onClick={() => getParticularProductDetail(product.id)}
+            >
+              <p>{product.title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </QuizContextProvider>
